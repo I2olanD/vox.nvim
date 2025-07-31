97c6f24030fec0a72656f4cca99a998c55aad9d0
Roland Wallner <rolandolah@icloud.com>
2025-07-31 09:08:43 +0200

    feat: Refactor

--- plugin/vox.lua
+++ plugin/vox.lua
@@ -8,11 +8,3 @@ if vim.g.loaded_vox then
 end
 
 vim.g.loaded_vox = 1
-
-local main_module_ok, vox = pcall(require, "vox")
-if not main_module_ok then
-  vim.notify("Vox: Failed to load main module. Plugin may not function correctly.", vim.log.levels.ERROR)
-else
-  -- Auto-setup with default configuration
-  vox.setup()
-end
